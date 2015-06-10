d205023597a70b2e6970bca47b36896f72f493c6
Peter Gui <pguitemp@gmail.com>
2015-06-09 22:34:56 -0400

    Update prime_analysis.py

--- prime_analysis.py
+++ prime_analysis.py
@@ -1,4 +1,7 @@
 def prime_list(number):
+    """Given an integer of 2 or greater this function will output a list of binary
+    indicating which numbers that integer is divisible by"""
+    
     lyst=[]
     for x in range(2,((number/2)+1)):
         if number%x == 0:
@@ -9,6 +12,8 @@ def prime_list(number):
 
 
 def binary_expansion(primeList):
+    """This function converts a list of binary into a number and divides it by a weight to keep the higher inputs
+    at the same relative level as the original inputs"""
     string = ''.join([str(x) for x in primeList])
     try:
         value=int(string,2)
@@ -18,8 +23,7 @@ def binary_expansion(primeList):
     
     return value/weight
 
-
-binaryExpansion=[]
+#this loop runs the functions on all the integers from 2 to 99 (notice the prime inputs come out as 0.0)
 for n in range(2,100):
     value=binary_expansion(prime_list(n))
-    binaryExpansion.append(value)
+    print n, value
